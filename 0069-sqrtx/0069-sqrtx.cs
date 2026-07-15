b82fcc6d38b23f3af23973bba00cb47f01bbd080
badhmaprawin-36 <badhmaprawin@gmail.com>
2026-07-15 21:09:16 +0530

    Time: 0 ms (100%), Space: 29.3 MB (21.67%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MySqrt(int x) {
+        if (x==0)
+        return 0;
+        long r=x;
+        while(r*r>x){
+            r=(r+x/r)/2;
+            
+        }
+        return (int)r;
+
+        
+    }
+}
